f8d86dd3c610b2f7d345c3831909525f6633b228
notkulonme <nricsi006@gmail.com>
2026-01-12 18:04:17 +0100

    initialize spring boot project

---
+++ backend/freshPotato/src/main/java/hu/pogany/freshPotato/FreshPotatoApplication.java
@@ -0,0 +1,13 @@
+package hu.pogany.freshPotato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreshPotatoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreshPotatoApplication.class, args);
+	}
+
+}
---
+++ backend/freshPotato/src/test/java/hu/pogany/freshPotato/FreshPotatoApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.pogany.freshPotato;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreshPotatoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
