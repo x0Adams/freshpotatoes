77ac80b044792076267a8c179cb8698090ef029a
Gyori Adam <adam.gyori02@gmail.com>
2026-02-16 23:49:53 +0100

    renaming pojos package to model =)

--- freshPotatoes/src/main/java/hu/freshpotatoes/Main.java
+++ freshPotatoes/src/main/java/hu/freshpotatoes/Main.java
@@ -1,6 +1,16 @@
 package hu.freshpotatoes;
 
+import hu.freshpotatoes.dao.DatabaseConnection;
+
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+        DatabaseConnection databaseConnection = new DatabaseConnection();
+
+        if (databaseConnection.getConnection() != null) {
+            System.out.println("Successfully connected to MariaDB!");
+            System.out.println("Driver Name: " + databaseConnection.getConnection().getMetaData().getDriverName());
+        }
     }
 }
