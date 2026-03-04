51de1fbd41478065e19497d4bbc972cfd8df518d
Gyori Adam <adam.gyori02@gmail.com>
2026-03-04 23:54:26 +0100

    basic spring boot project initialization

---
+++ freshPotatoes/src/test/java/hu/freshPotatoes/FreshPotatoesApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.freshPotatoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreshPotatoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
