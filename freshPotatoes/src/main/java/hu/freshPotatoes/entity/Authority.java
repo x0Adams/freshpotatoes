5cc7f2c3a99940c7dc44911a4a0016974ebc3414
Gyori Adam <adam.gyori02@gmail.com>
2026-03-08 22:55:25 +0100

    working spring boot hehe

---
+++ freshPotatoes/src/main/java/hu/freshPotatoes/entity/Authority.java
@@ -0,0 +1,22 @@
+package hu.freshPotatoes.entity;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "authorities", schema = "fresh_potatoes")
+public class Authority {
+    @EmbeddedId
+    private AuthorityId id;
+
+    public AuthorityId getId() {
+        return id;
+    }
+
+    public void setId(AuthorityId id) {
+        this.id = id;
+    }
+
+    //TODO [Reverse Engineering] generate columns from DB
+}
---
+++ freshPotatoes/src/main/java/hu/freshPotatoes/repository/MovieRepository.java
@@ -0,0 +1,9 @@
+package hu.freshPotatoes.repository;
+
+import hu.freshPotatoes.entity.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MovieRepository extends JpaRepository<Movie, Integer> {
+}
