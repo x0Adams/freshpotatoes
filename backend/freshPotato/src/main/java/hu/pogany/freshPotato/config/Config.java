72082e5a6baa926def9c61ff6ae9afce62cbacff
notkulonme <nricsi006@gmail.com>
2026-04-02 16:52:55 +0200

    add OpenAPI documentation configuration; implement custom OpenAPI bean in Config.java and add springdoc-openapi dependency in pom.xml

--- backend/freshPotato/src/main/java/hu/pogany/freshPotato/config/Config.java
+++ backend/freshPotato/src/main/java/hu/pogany/freshPotato/config/Config.java
@@ -1,5 +1,10 @@
 package hu.pogany.freshPotato.config;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -10,5 +15,20 @@ import java.security.NoSuchAlgorithmException;
 @Import(SecurityConfig.class)
 @Configuration
 public class Config {
-    
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("freshpotatoes docs")
+                        .version("in development")
+                        .description("Ádám Győri and Nagy Richárd's final project")
+                )
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"));
+    }
 }
