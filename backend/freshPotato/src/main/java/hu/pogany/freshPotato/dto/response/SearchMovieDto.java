749d473154448c26fa57f8e84dd0d3f93d9463c4
notkulonme <nricsi006@gmail.com>
2026-04-14 21:09:17 +0200

    enhance SearchMovieDto; add genres and releaseDate fields to improve movie search response

--- backend/freshPotato/src/main/java/hu/pogany/freshPotato/dto/response/SearchMovieDto.java
+++ backend/freshPotato/src/main/java/hu/pogany/freshPotato/dto/response/SearchMovieDto.java
@@ -1,4 +1,13 @@
 package hu.pogany.freshPotato.dto.response;
 
-public record SearchMovieDto(String id, String name, String posterPath) {
+import java.time.LocalDate;
+import java.util.Set;
+
+public record SearchMovieDto(
+        String id,
+        String name,
+        String posterPath,
+        Set<GenreDto> genres,
+        LocalDate releaseDate
+) {
 }
